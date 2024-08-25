c3b7cd712186bd7afcd281b6dc197ee668a07f5f
Ahmad <a@gmail.com>
2024-08-26 01:14:29 +0700

    commit kedua

--- test/specs/test.demo.js
+++ test/specs/test.demo.js
@@ -1,7 +1,7 @@
 
 
 describe('Test saucedemo', () => {
-    it('Test 1 - Login succesfully', async () => {
+    it('Test 1 - Sauce Demo Test', async () => {
         await browser.url("https://www.saucedemo.com/")
         
         const usernametxtbox = await browser.$("#user-name")
@@ -11,7 +11,7 @@ describe('Test saucedemo', () => {
         const addToCartBtn1 = await browser.$("#add-to-cart-sauce-labs-backpack")
         const addToCartBtn2 = await browser.$("#add-to-cart-sauce-labs-bolt-t-shirt")
         const cartIcon = await browser.$("#shopping_cart_container")
-
+        const inventoryItem = await browser.$("#cart_item")
 
         await usernametxtbox.addValue("standard_user")
         await passwordtxtbox.addValue("secret_sauce")
@@ -27,6 +27,8 @@ describe('Test saucedemo', () => {
 
         await browser.pause(3000)
         await expect(browser).toHaveUrl('https://www.saucedemo.com/cart.html')
-
+        await inventoryItem =='Sauce Labs Backpack'
+        await inventoryItem == 'Sauce Labs Bolt T-Shirt'
+        
     });
 });
