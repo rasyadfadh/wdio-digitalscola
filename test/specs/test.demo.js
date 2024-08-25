ae80f1a857f3f3c1f7b27b7bf7e10c3a1da13dcf
Ahmad <a@gmail.com>
2024-08-25 21:35:45 +0700

    commit tugas sesi 9

---
+++ test/specs/test.demo.js
@@ -0,0 +1,32 @@
+
+
+describe('Test saucedemo', () => {
+    it('Test 1 - Login succesfully', async () => {
+        await browser.url("https://www.saucedemo.com/")
+        
+        const usernametxtbox = await browser.$("#user-name")
+        const passwordtxtbox = await browser.$("#password")
+        const loginbtn = await browser.$('//input[@type="submit"]')
+
+        const addToCartBtn1 = await browser.$("#add-to-cart-sauce-labs-backpack")
+        const addToCartBtn2 = await browser.$("#add-to-cart-sauce-labs-bolt-t-shirt")
+        const cartIcon = await browser.$("#shopping_cart_container")
+
+
+        await usernametxtbox.addValue("standard_user")
+        await passwordtxtbox.addValue("secret_sauce")
+        await loginbtn.click()
+   
+        await browser.pause(3000)
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+        await expect(browser).toHaveTitle('Swag Labs')
+
+        await addToCartBtn1.click()
+        await addToCartBtn2.click()
+        await cartIcon.click()
+
+        await browser.pause(3000)
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/cart.html')
+
+    });
+});
